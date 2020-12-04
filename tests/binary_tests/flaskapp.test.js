6879b5dd5401166f9816e3ab8bf29c9069a3a2ec
Doug <dcdenu4@gmail.com>
2020-12-04 13:51:49 -0500

    Add await in front of call to fix tests

--- tests/binary_tests/flaskapp.test.js
+++ tests/binary_tests/flaskapp.test.js
@@ -13,7 +13,7 @@ jest.setTimeout(250000); // This test is slow in CI
 const isDevMode = true; // otherwise need to mock process.resourcesPath
 beforeAll(async () => {
   const binaries = await findInvestBinaries(isDevMode);
-  createPythonFlaskProcess(binaries.server, isDevMode);
+  const port = await createPythonFlaskProcess(binaries.server, isDevMode);
   // In the CI the flask app takes more than 10x as long to startup.
   // Especially so on macos.
   // So, allowing many retries, especially because the error
